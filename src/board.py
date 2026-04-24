e3a3791642dd25e5fb0fc1cc6e00ffd35eeac06c
Tymon Dera <91739652+tdera6@users.noreply.github.com>
2026-04-24 16:14:40 +0200

    feat(board): setup starting position (#2)
    * fix(board): add missing constants
    
    * test(board): test pieces constants
    
    * feat(board): add starting position setup function
    
    * test(board): test board starting position

--- src/board.py
+++ src/board.py
@@ -13,8 +13,46 @@ class Board:
     QUEEN = 5
     KING = 6
 
+    WHITE_PAWN = PAWN * WHITE
+    BLACK_PAWN = PAWN * BLACK
+    WHITE_KNIGHT = KNIGHT * WHITE
+    BLACK_KNIGHT = KNIGHT * BLACK
+    WHITE_BISHOP = BISHOP * WHITE
+    BLACK_BISHOP = BISHOP * BLACK
+    WHITE_ROOK = ROOK * WHITE
+    BLACK_ROOK = ROOK * BLACK
+    WHITE_QUEEN = QUEEN * WHITE
+    BLACK_QUEEN = QUEEN * BLACK
+    WHITE_KING = KING * WHITE
+    BLACK_KING = KING * BLACK
+
     def __init__(self):
         self.squares = [self.EMPTY] * 128
 
         self.turn = self.WHITE
     
+    def setup_starting_position(self):
+        # Set up the initial position of pieces on the board
+        self.squares[0] = self.WHITE_ROOK
+        self.squares[1] = self.WHITE_KNIGHT
+        self.squares[2] = self.WHITE_BISHOP
+        self.squares[3] = self.WHITE_QUEEN
+        self.squares[4] = self.WHITE_KING
+        self.squares[5] = self.WHITE_BISHOP
+        self.squares[6] = self.WHITE_KNIGHT
+        self.squares[7] = self.WHITE_ROOK
+
+        for i in range(16, 24):
+            self.squares[i] = self.WHITE_PAWN
+
+        for i in range(96, 104):
+            self.squares[i] = self.BLACK_PAWN
+
+        self.squares[112] = self.BLACK_ROOK
+        self.squares[113] = self.BLACK_KNIGHT
+        self.squares[114] = self.BLACK_BISHOP
+        self.squares[115] = self.BLACK_QUEEN
+        self.squares[116] = self.BLACK_KING
+        self.squares[117] = self.BLACK_BISHOP
+        self.squares[118] = self.BLACK_KNIGHT
+        self.squares[119] = self.BLACK_ROOK
--- tests/test_board.py
+++ tests/test_board.py
@@ -9,3 +9,25 @@ def test_board_starts_with_white_turn():
     board = Board()
 
     assert board.turn == board.WHITE
+
+def test_board_pieces_constants():
+    assert Board.WHITE_PAWN == 1
+    assert Board.BLACK_BISHOP == -3
+    assert Board.WHITE_KING == 6
+    assert Board.BLACK_QUEEN == -5
+
+def test_board_setup_starting_position():
+    board = Board()
+    board.setup_starting_position()
+
+    # Check some key squares for white pieces
+    assert board.squares[0] == Board.WHITE_ROOK
+    assert board.squares[1] == Board.WHITE_KNIGHT
+    assert board.squares[4] == Board.WHITE_KING
+    assert board.squares[16] == Board.WHITE_PAWN
+
+    # Check some key squares for black pieces
+    assert board.squares[112] == Board.BLACK_ROOK
+    assert board.squares[113] == Board.BLACK_KNIGHT
+    assert board.squares[116] == Board.BLACK_KING
+    assert board.squares[96] == Board.BLACK_PAWN
