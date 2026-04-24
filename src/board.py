008f1e8dd4e23e1f2ac4e4a710c7d68393352665
tdera6 <dera.inf@wp.pl>
2026-04-24 16:00:38 +0200

    feat(board): add starting position setup function

--- src/board.py
+++ src/board.py
@@ -31,3 +31,28 @@ class Board:
 
         self.turn = self.WHITE
     
+    def setup_starting_position(self):
+        # Set up the initial position of pieces on the board
+        self.squares[0] = self.WHITE_ROOK
+        self.squares[1] = self.WHITE_KNIGHT
+        self.squares[2] = self.WHITE_BISHOP
+        self.squares[3] = self.WHITE_QUEEN
+        self.squares[4] = self.WHITE_KING
+        self.squares[5] = self.WHITE_BISHOP
+        self.squares[6] = self.WHITE_KNIGHT
+        self.squares[7] = self.WHITE_ROOK
+
+        for i in range(16, 24):
+            self.squares[i] = self.WHITE_PAWN
+
+        for i in range(96, 104):
+            self.squares[i] = self.BLACK_PAWN
+
+        self.squares[112] = self.BLACK_ROOK
+        self.squares[113] = self.BLACK_KNIGHT
+        self.squares[114] = self.BLACK_BISHOP
+        self.squares[115] = self.BLACK_QUEEN
+        self.squares[116] = self.BLACK_KING
+        self.squares[117] = self.BLACK_BISHOP
+        self.squares[118] = self.BLACK_KNIGHT
+        self.squares[119] = self.BLACK_ROOK
