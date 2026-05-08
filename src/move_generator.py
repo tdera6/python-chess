ed0c72cd2a9e687fa3fe38c08512729adcb11541
tdera6 <dera.inf@wp.pl>
2026-05-08 20:44:15 +0200

    feat(pawn): add starting two squares move for pawns

--- src/move_generator.py
+++ src/move_generator.py
@@ -3,7 +3,7 @@ from src.move import Move
 
 class MoveGenerator:
     def __init__(self, board: Board):
-        self.board = Board
+        self.board = board
 
     def generate_moves(self) -> list[Move]:
         moves = []
@@ -16,12 +16,38 @@ class MoveGenerator:
 
             if piece == Board.WHITE_PAWN:
                 self.generate_white_pawn_moves(square, piece, moves)
+            elif piece == Board.BLACK_PAWN:
+                self.generate_black_pawn_moves(square, piece, moves)
         
         return moves
     
     def generate_white_pawn_moves(self, square: int, piece: int, moves: list[Move]):
         one_square_ahead = square + 16
-        
+        two_squares_ahead = square + 32
+
+        # Basic one square move
         if not (one_square_ahead & 0x88) and self.board.squares[one_square_ahead] == Board.EMPTY:
             moves.append(Move(from_square=square, to_square=one_square_ahead, piece_moved=piece))
+
+        # Check if pawn is in second row
+        if square // 16 == 1:
+            # Check if two squares ahead are empty
+            if self.board.squares[one_square_ahead] == Board.EMPTY\
+            and self.board.squares[two_squares_ahead] == Board.EMPTY:
+                moves.append(Move(from_square=square, to_square=two_squares_ahead, piece_moved=piece))
+
+    def generate_black_pawn_moves(self, square: int, piece: int, moves: list[Move]):
+        one_square_ahead = square - 16
+        two_squares_ahead = square - 32
+
+        # Basic one square move
+        if not (one_square_ahead & 0x88) and self.board.squares[one_square_ahead] == Board.EMPTY:
+            moves.append(Move(from_square=square, to_square=one_square_ahead, piece_moved=piece))
+
+        # Check if pawn is in seventh row
+        if square // 16 == 6:
+            # Check if two squares ahead are empty
+            if self.board.squares[one_square_ahead] == Board.EMPTY\
+            and self.board.squares[two_squares_ahead] == Board.EMPTY:
+                moves.append(Move(from_square=square, to_square=two_squares_ahead, piece_moved=piece))
     
