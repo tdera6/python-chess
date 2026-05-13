a410e418c797e38b13cadcc3bd713506c625a31a
Tymon Dera <91739652+tdera6@users.noreply.github.com>
2026-05-13 21:42:13 +0200

    feat: add basic move generator (#5)
    * feat(move-generator): add move class
    
    * feat(move-generator): add move generator class
    
    * feat(pawn): add starting two squares move for pawns
    
    * fix(move-generator): move generator generates moves only for the player whose turn it is now
    
    * test(pawn): test basic pawn movement (without capturing)
    
    * feat(move): add move square notation (e.g. e2-e4)
    
    * test(pawn): fix pawn moves test without capturing
    
    * feat(move-generator): add functions to check if piece on occupied square is enemy or friend
    
    * feat(pawn): add capturing (without en passant)
    
    * test(pawn): split pawn tests into single pawn test and multiple pawns integration test
    
    * feat(knight): add knight movement (WIP: write tests)
    
    * test(knight): test knight movement
    
    * feat(move-generator): add helper function to generate queen, bishop and rook moves
    
    * feat(bishop): add and test bishop movement
    
    * feat(rook): add and test rook movement
    
    * feat(queen): add and test bishop movement
    
    * feat(king): add king movement (without castling)
    
    * refactor: change variables names in tests

---
+++ src/move_generator.py
@@ -0,0 +1,184 @@
+from src.board import Board
+from src.move import Move
+
+class MoveGenerator:
+    def __init__(self, board: Board):
+        self.board = board
+
+    def generate_moves(self) -> list[Move]:
+        moves = []
+
+        for square in range(128):
+            if square & 0x88:
+                continue
+
+            piece = self.board.squares[square]
+
+            # Generate moves for white if there is their turn
+            if self.board.turn == Board.WHITE:
+                if piece == Board.WHITE_PAWN:
+                    self.generate_white_pawn_moves(square, piece, moves)
+                elif piece == Board.WHITE_KNIGHT:
+                    self.generate_knight_moves(square, piece, moves)
+                elif piece == Board.WHITE_BISHOP:
+                    self.generate_bishop_moves(square, piece, moves)
+                elif piece == Board.WHITE_ROOK:
+                    self.generate_rook_moves(square, piece, moves)
+                elif piece == Board.WHITE_QUEEN:
+                    self.generate_queen_moves(square, piece, moves)
+                elif piece == Board.WHITE_KING:
+                    self.generate_king_moves(square, piece, moves)
+
+            
+            # Generate moves for black if there is their turn
+            elif self.board.turn == Board.BLACK:
+                if piece == Board.BLACK_PAWN:
+                    self.generate_black_pawn_moves(square, piece, moves)
+                elif piece == Board.BLACK_KNIGHT:
+                    self.generate_knight_moves(square, piece, moves)
+                elif piece == Board.BLACK_BISHOP:
+                    self.generate_bishop_moves(square, piece, moves)
+                elif piece == Board.BLACK_ROOK:
+                    self.generate_rook_moves(square, piece, moves)
+                elif piece == Board.BLACK_QUEEN:
+                    self.generate_queen_moves(square, piece, moves)
+                elif piece == Board.BLACK_KING:
+                    self.generate_king_moves(square, piece, moves)
+        
+        return moves
+
+    # Chec if piece is the same color as the moving piece
+    def is_friendly(self, piece: int) -> bool:
+        turn = self.board.turn
+
+        if piece == Board.EMPTY:
+            return False
+        
+        return  (turn == Board.WHITE and piece > 0) or\
+                (turn == Board.BLACK and piece < 0)
+    
+    # Check if piece is opposite color from the moving piece
+    def is_enemy(self, piece: int) -> bool:
+        turn = self.board.turn
+
+        if piece == Board.EMPTY:
+            return False
+        
+        return  (turn == Board.WHITE and piece < 0) or\
+                (turn == Board.BLACK and piece > 0)
+    
+    def generate_white_pawn_moves(self, square: int, piece: int, moves: list[Move]):
+        one_square_ahead = square + 16
+        two_squares_ahead = square + 32
+
+        # Basic one square move
+        if not (one_square_ahead & 0x88) and self.board.squares[one_square_ahead] == Board.EMPTY:
+            moves.append(Move(from_square=square, to_square=one_square_ahead, piece_moved=piece))
+
+        # Check if pawn is in second row
+        if square // 16 == 1:
+            # Check if two squares ahead are empty
+            if self.board.squares[one_square_ahead] == Board.EMPTY\
+            and self.board.squares[two_squares_ahead] == Board.EMPTY:
+                moves.append(Move(from_square=square, to_square=two_squares_ahead, piece_moved=piece))
+
+        # Capturing
+        left_capture = square + 15
+        right_capture = square + 17
+
+        if not (left_capture & 0x88) and self.is_enemy(self.board.squares[left_capture]):
+            moves.append(Move(from_square=square, to_square=left_capture, piece_moved=piece, piece_captured=self.board.squares[left_capture]))
+
+        if not (right_capture & 0x88) and self.is_enemy(self.board.squares[right_capture]):
+            moves.append(Move(from_square=square, to_square=right_capture, piece_moved=piece, piece_captured=self.board.squares[right_capture]))
+
+    def generate_black_pawn_moves(self, square: int, piece: int, moves: list[Move]):
+        one_square_ahead = square - 16
+        two_squares_ahead = square - 32
+
+        # Basic one square move
+        if not (one_square_ahead & 0x88) and self.board.squares[one_square_ahead] == Board.EMPTY:
+            moves.append(Move(from_square=square, to_square=one_square_ahead, piece_moved=piece))
+
+        # Check if pawn is in seventh row
+        if square // 16 == 6:
+            # Check if two squares ahead are empty
+            if self.board.squares[one_square_ahead] == Board.EMPTY\
+            and self.board.squares[two_squares_ahead] == Board.EMPTY:
+                moves.append(Move(from_square=square, to_square=two_squares_ahead, piece_moved=piece))
+
+        # Capturing
+        left_capture = square - 15
+        right_capture = square - 17
+
+        if not (left_capture & 0x88) and self.is_enemy(self.board.squares[left_capture]):
+            moves.append(Move(from_square=square, to_square=left_capture, piece_moved=piece, piece_captured=self.board.squares[left_capture]))
+
+        if not (right_capture & 0x88) and self.is_enemy(self.board.squares[right_capture]):
+            moves.append(Move(from_square=square, to_square=right_capture, piece_moved=piece, piece_captured=self.board.squares[right_capture]))
+    
+    def generate_knight_moves(self, square: int, piece: int, moves: list[Move]):
+        directions = {33, 18, -14, -31, -33, -18, 14, 31}
+
+        for direction in directions:
+            target_square = square + direction
+            
+            if not (target_square & 0x88):
+                target_piece = self.board.squares[target_square]
+                
+                if target_piece == Board.EMPTY:
+                    moves.append(Move(from_square=square, to_square=target_square, piece_moved=piece))
+                elif self.is_enemy(target_piece):
+                    moves.append(Move(from_square=square, to_square=target_square, piece_moved=piece, piece_captured=target_piece))
+    
+    # Helper function to generate Queen, Bishop and Rook moves
+    def sliding_pieces(self, square: int , piece: int, directions: list[int], moves: list[Move]):
+
+        from_square = square
+
+        for direction in directions:
+            to_square = from_square
+            while True:
+                to_square += direction
+
+                if to_square & 0x88:
+                    break
+                
+                piece_on_to_square = self.board.squares[to_square]
+                if piece_on_to_square == Board.EMPTY:
+                    moves.append(Move(from_square, to_square, piece))
+                elif self.is_enemy(piece_on_to_square):
+                    moves.append(Move(from_square, to_square, piece, piece_captured=piece_on_to_square))
+                    break
+                elif self.is_friendly(piece_on_to_square):
+                    break
+
+    def generate_bishop_moves(self, square: int, piece: int, moves: list[Move]):
+        directions = [15, 17, -15, -17]
+        self.sliding_pieces(square, piece, directions, moves)
+
+    def generate_rook_moves(self, square: int, piece: int, moves: list[Move]):
+        directions = [1, -1, 16, -16]
+        self.sliding_pieces(square, piece, directions, moves)
+
+    def generate_queen_moves(self, square: int, piece: int, moves: list[Move]):
+        directions = [15, 17, -15, -17, 1, -1, 16, -16]
+        self.sliding_pieces(square, piece, directions, moves)
+
+    def generate_king_moves(self, square: int, piece: int, moves: list[Move]):
+        directions = [1, -17, -16, -15, -1, 15, 16, 17]
+
+        for direction in directions:
+            to_square = square + direction
+            
+            if to_square & 0x88:
+                continue
+
+            piece_on_to_square = self.board.squares[to_square]
+
+            if piece_on_to_square == Board.EMPTY:
+                moves.append(Move(from_square=square, to_square=to_square, piece_moved=piece))
+            elif self.is_enemy(piece_on_to_square):
+                moves.append(Move(from_square=square, to_square=to_square, piece_moved=piece, piece_captured=piece_on_to_square))
+            elif self.is_friendly(piece_on_to_square):
+                continue
---
+++ tests/test_move_generator.py
@@ -0,0 +1,173 @@
+import pytest
+from src.board import Board
+from src.move_generator import MoveGenerator
+
+@pytest.mark.parametrize("test_input, target_square, expected_number_of_moves", [
+    ("8/8/8/8/8/8/3P4/8 w - - 0 0", 0x13, 2),
+    ("8/3p4/8/8/8/8/8/8 b - - 0 0", 0x63, 2),
+    ("8/8/8/8/8/4p3/4P3/8 w - - 0 0", 0x14, 0),
+    ("8/8/8/8/8/4p3/4P3/8 b - - 0 0", 0x24, 0),
+])
+def test_single_pawn_movement_without_capturing(test_input: str, target_square: int, expected_number_of_moves: int):
+    board = Board()
+    board.load_FEN(test_input)
+    generator = MoveGenerator(board)
+    moves = []
+
+    if board.turn == Board.WHITE:
+        generator.generate_white_pawn_moves(target_square, Board.WHITE_PAWN, moves)
+    elif board.turn == Board.BLACK:
+        generator.generate_black_pawn_moves(target_square, Board.BLACK_PAWN, moves)
+    
+    assert len(moves) == expected_number_of_moves
+
+@pytest.mark.parametrize("test_input, expected_number_of_moves", [
+    ("8/8/8/8/8/2p1p3/3P4/8 w - - 0 0", 4),
+    ("8/8/8/8/8/2p1p3/3P4/8 b - - 0 0", 4),
+    ("8/8/8/8/8/pppppppp/PPPPPPPP/8 w - - 0 0", 14),
+    ("8/8/8/8/8/pppppppp/PPPPPPPP/8 b - - 0 0", 14),
+    ("8/2p5/2Ppp3/p3Ppp1/1p4P1/1P5P/8/8 w - - 0 0", 3),
+    ("8/2p5/2Ppp3/p3Ppp1/1p4P1/1P5P/8/8 b - - 0 0", 5),
+    ("8/6p1/2p1p3/3P4/2P5/1P6/6PP/8 w - - 0 0", 9),
+    ("8/6p1/2p1p3/3P4/2P5/1P6/6PP/8 b - - 0 0", 6),
+])
+def test_multiple_pawns_integration_without_en_passant(test_input, expected_number_of_moves):
+    board = Board()
+    board.load_FEN(test_input)
+    generator = MoveGenerator(board)
+
+    moves = generator.generate_moves()
+
+    if board.turn == Board.WHITE:
+        pawns_moves = [move for move in moves if move.piece_moved == Board.WHITE_PAWN]
+    elif board.turn == Board.BLACK:
+        pawns_moves = [move for move in moves if move.piece_moved == Board.BLACK_PAWN]
+
+
+    assert len(pawns_moves) == expected_number_of_moves
+
+@pytest.mark.parametrize("test_input, expected_number_of_moves", [
+    ("8/8/8/8/8/2N5/8/8 w - - 0 0", 8),
+    ("8/8/8/8/8/8/8/N7 w - - 0 0", 2),
+    ("8/8/8/8/8/8/8/1N6 w - - 0 0", 3),
+    ("8/8/8/8/8/2n5/8/8 b - - 0 0", 8),
+    ("8/8/8/8/8/8/8/n7 b - - 0 0", 2),
+    ("8/8/8/8/8/8/8/1n6 b - - 0 0", 3),
+    ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - - 0 0", 4),
+    ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b - - 0 0", 4),
+    ("8/8/8/8/2p1p3/1p3p2/3N4/8 w - - 0 0", 6),
+    ("8/8/8/8/2P1p3/1P3P2/3n4/8 b - - 0 0", 5),
+])
+def test_knights_movement(test_input, expected_number_of_moves):
+    board = Board()
+    board.load_FEN(test_input)
+    generator = MoveGenerator(board)
+
+    moves = generator.generate_moves()
+
+    if board.turn == Board.WHITE:
+        knight_moves = [move for move in moves if move.piece_moved == Board.WHITE_KNIGHT]
+    elif board.turn == Board.BLACK:
+        knight_moves = [move for move in moves if move.piece_moved == Board.BLACK_KNIGHT]
+
+
+    assert len(knight_moves) == expected_number_of_moves
+
+@pytest.mark.parametrize("test_input, expected_number_of_moves", [
+    ("B7/8/8/8/8/8/8/8 w - - 0 0", 7),
+    ("1B6/8/8/8/8/8/8/8 w - - 0 0", 7),
+    ("8/8/8/8/4B3/8/8/8 w - - 0 0", 13),
+    ("8/8/8/3P1P2/4B3/8/8/8 w - - 0 0", 6),
+    ("8/8/8/3P1P2/4B3/8/2p5/8 w - - 0 0", 5),
+    ("8/8/8/2P5/1B6/2p5/8/8 w - - 0 0", 3),
+    ("8/8/8/8/8/8/1P6/B7 w - - 0 0", 0),
+    ("b7/8/8/8/8/8/8/8 b - - 0 0", 7),
+    ("1b6/8/8/8/8/8/8/8 b - - 0 0", 7),
+    ("8/8/8/8/4b3/8/8/8 b - - 0 0", 13),
+    ("8/8/8/3p1p2/4b3/8/8/8 b - - 0 0", 6),
+    ("8/8/8/3p1p2/4b3/8/2P5/8 b - - 0 0", 5),
+    ("8/8/8/2p5/1b6/2P5/8/8 b - - 0 0", 3),
+    ("8/8/8/8/8/8/1p6/b7 b - - 0 0", 0),
+])
+def test_bishop_movement(test_input, expected_number_of_moves):
+    board = Board()
+    board.load_FEN(test_input)
+    generator = MoveGenerator(board)
+
+    moves = generator.generate_moves()
+
+    if board.turn == Board.WHITE:
+        bishop_moves = [move for move in moves if move.piece_moved == Board.WHITE_BISHOP]
+    elif board.turn == Board.BLACK:
+        bishop_moves = [move for move in moves if move.piece_moved == Board.BLACK_BISHOP]
+
+
+    assert len(bishop_moves) == expected_number_of_moves
+
+@pytest.mark.parametrize("test_input, expected_number_of_moves", [
+    ("R7/8/8/8/8/8/8/8 w - - 0 0", 14),
+    ("8/8/1R6/8/8/8/8/8 w - - 0 0", 14),
+    ("8/8/4P3/4RP2/8/8/8/8 w - - 0 0", 8),
+    ("8/8/4p3/4Rp2/8/8/8/8 w - - 0 0", 10),
+    ("8/8/8/8/8/5r2/8/8 b - - 0 0", 14),
+    ("8/8/8/8/8/8/8/r7 b - - 0 0", 14),
+    ("8/8/8/P7/rP6/P7/8/8 b - - 0 0", 3),
+    ("8/8/8/p7/rp6/p7/8/8 b - - 0 0", 0),
+])
+def test_rook_movement(test_input, expected_number_of_moves):
+    board = Board()
+    board.load_FEN(test_input)
+    generator = MoveGenerator(board)
+
+    moves = generator.generate_moves()
+
+    if board.turn == Board.WHITE:
+        rook_moves = [move for move in moves if move.piece_moved == Board.WHITE_ROOK]
+    elif board.turn == Board.BLACK:
+        rook_moves = [move for move in moves if move.piece_moved == Board.BLACK_ROOK]
+
+
+    assert len(rook_moves) == expected_number_of_moves
+
+@pytest.mark.parametrize("test_input, expected_number_of_moves", [
+    ("8/8/8/8/1Q6/8/8/8 w - - 0 0", 23),
+    ("8/8/3p4/1P6/1Q1p4/8/3p4/8 w - - 0 0", 12),
+    ("Q7/PP6/8/8/8/8/8/8 w - - 0 0", 7),
+    ("8/8/3ppp2/3pqp2/3ppp2/8/8/8 b - - 0 0", 0),
+    ("8/2PpP3/2pQp3/2PpP3/8/8/8/8 w - - 0 0", 4),
+])
+def test_queen_movement(test_input, expected_number_of_moves):
+    board = Board()
+    board.load_FEN(test_input)
+    generator = MoveGenerator(board)
+
+    moves = generator.generate_moves()
+
+    if board.turn == Board.WHITE:
+        queen_moves = [move for move in moves if move.piece_moved == Board.WHITE_QUEEN]
+    elif board.turn == Board.BLACK:
+        queen_moves = [move for move in moves if move.piece_moved == Board.BLACK_QUEEN]
+
+
+    assert len(queen_moves) == expected_number_of_moves
+
+@pytest.mark.parametrize("test_input, expected_number_of_moves", [
+    ("8/8/8/8/8/2K5/8/8 w - - 0 0", 8),
+    ("8/8/8/8/1PPP4/1pKp4/8/8 w - - 0 0", 5),
+    ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - - 0 0", 0),
+    ("7k/8/8/8/8/8/8/8 b - - 0 0", 3),
+    ("8/8/8/6pp/6Pk/7p/8/8 b - - 0 0", 2),
+])
+def test_king_movement(test_input, expected_number_of_moves):
+    board = Board()
+    board.load_FEN(test_input)
+    generator = MoveGenerator(board)
+
+    moves = generator.generate_moves()
+
+    if board.turn == Board.WHITE:
+        king_moves = [move for move in moves if move.piece_moved == Board.WHITE_KING]
+    elif board.turn == Board.BLACK:
+        king_moves = [move for move in moves if move.piece_moved == Board.BLACK_KING]
+
+    assert len(king_moves) == expected_number_of_moves
