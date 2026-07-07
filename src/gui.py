6f8c4fb9640f3ad20e0b5c3c3407090fbf7e5553
Tymon Dera <91739652+tdera6@users.noreply.github.com>
2026-07-07 22:52:08 +0200

    feat: add graphic user interface in pygame (#11)
    Description:
    Added a GUI using pygame so players can interact with the board. Player
    can only select pieces that have legal moves. The GUI also helps players
    by showing where they can move.
    
    Changes made:
    * Added pygame main loop and drawing the board.
    * Added .png images for chess pieces.
    * Added highlighting when you click on a piece.
    * Added move indicators (dots for normal moves, rings for captures).
    * Connected GUI with move generator to block illegal moves.

---
+++ src/gui.py
@@ -0,0 +1,189 @@
+import pygame
+from pathlib import Path
+from src.board import Board
+from src.move_generator import MoveGenerator
+
+WIDTH = 1000
+HEIGHT = 1000
+
+SQUARE_SIZE = WIDTH / 8
+
+BASE_DIR = Path(__file__).parent.parent.absolute()
+
+PIECES = [
+    "white-pawn",
+    "white-knight",
+    "white-bishop",
+    "white-rook",
+    "white-queen",
+    "white-king",
+    "black-king",
+    "black-queen",
+    "black-rook",
+    "black-bishop",
+    "black-knight",
+    "black-pawn",
+]
+
+
+class GUI:
+    def __init__(self, board: Board):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.piece_images = [None]
+        self.dot_image = None
+        self.circle_image = None
+        self.board = board
+        self.load_images()
+        self.clicked_squares = []
+        self.possible_moves = MoveGenerator(self.board).generate_legal_moves()
+
+    def load_images(self):
+        for piece in PIECES:
+            img = pygame.image.load(f"{BASE_DIR}/assets/images/{piece}.png")
+            scaled_img = pygame.transform.scale(img, (SQUARE_SIZE, SQUARE_SIZE))
+            converted_alpha_img = pygame.Surface.convert_alpha(scaled_img)
+            self.piece_images.append(converted_alpha_img)
+
+        img = pygame.image.load(f"{BASE_DIR}/assets/images/dot.png")
+        scaled_img = pygame.transform.scale(img, (SQUARE_SIZE, SQUARE_SIZE))
+        converted_alpha_img = scaled_img.convert_alpha()
+        converted_alpha_img.set_alpha(64)
+        self.dot_image = converted_alpha_img
+
+        img = pygame.image.load(f"{BASE_DIR}/assets/images/circle.png")
+        scaled_img = pygame.transform.scale(img, (SQUARE_SIZE, SQUARE_SIZE))
+        converted_alpha_img = scaled_img.convert_alpha()
+        converted_alpha_img.set_alpha(64)
+        self.circle_image = converted_alpha_img
+
+    def display_pieces(self):
+        for row in range(8):
+            for column in range(8):
+                piece = self.board.squares[row * 16 + column]
+                if piece != 0:
+                    self.screen.blit(
+                        self.piece_images[piece],
+                        (
+                            (column * SQUARE_SIZE),
+                            (HEIGHT - (row + 1) * SQUARE_SIZE),
+                        ),
+                    )
+
+    def handle_click_detection(self, event):
+        clicked_square = self.square_click_detection(event)
+
+        if clicked_square == -1:
+            self.clicked_squares.clear()
+            return
+
+        if len(self.clicked_squares) == 0 and self.board.squares[clicked_square] == 0:
+            return
+
+        if clicked_square in self.clicked_squares:
+            self.clicked_squares.clear()
+            return
+
+        if (
+            len(self.clicked_squares) == 1
+            and self.board.squares[clicked_square] * self.board.turn > 0
+        ):
+            self.clicked_squares.clear()
+
+        self.clicked_squares.append(clicked_square)
+
+        if len(self.clicked_squares) == 2:
+            for move in self.possible_moves:
+                if (
+                    move.from_square == self.clicked_squares[0]
+                    and move.to_square == self.clicked_squares[1]
+                ):
+                    self.board.make_move(move)
+                    self.possible_moves = MoveGenerator(
+                        self.board
+                    ).generate_legal_moves()
+                    break
+
+            self.clicked_squares.clear()
+
+    def square_click_detection(self, event) -> int:
+        if event.button != 1:
+            return -1
+
+        x, y = event.pos
+        column = x // SQUARE_SIZE
+
+        # prevent row = 8 if player clicks on 0 px
+        row = min((HEIGHT - y) // SQUARE_SIZE, 7)
+
+        return int(row * 16 + column)
+
+    def main_loop(self):
+        running = True
+
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.handle_click_detection(event)
+
+            self.screen.fill("green")
+
+            for i in range(8):
+                for j in range(8):
+                    square_color = "#eeeed2" if (i + j) % 2 == 0 else "#769656"
+                    square_number_gui = (7 - j) * 16 + i
+
+                    piece_on_square = self.board.squares[square_number_gui]
+                    current_turn = self.board.turn
+
+                    pygame.draw.rect(
+                        self.screen,
+                        square_color,
+                        pygame.Rect(
+                            i * SQUARE_SIZE, j * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE
+                        ),
+                    )
+
+                    if (
+                        square_number_gui in self.clicked_squares
+                        and piece_on_square != 0
+                        and piece_on_square * current_turn > 0
+                        and any(
+                            move.from_square == square_number_gui
+                            for move in self.possible_moves
+                        )
+                    ):
+                        highlighted_square = pygame.Surface((SQUARE_SIZE, SQUARE_SIZE))
+                        highlighted_square.set_alpha(128)
+                        highlighted_square.fill("#a0c322")
+                        self.screen.blit(
+                            highlighted_square, (i * SQUARE_SIZE, j * SQUARE_SIZE)
+                        )
+
+                    if len(self.clicked_squares) == 1:
+                        for move in self.possible_moves:
+                            if (
+                                move.from_square == self.clicked_squares[0]
+                                and move.to_square == square_number_gui
+                            ):
+                                if move.piece_captured == 0:
+                                    self.screen.blit(
+                                        self.dot_image,
+                                        (i * SQUARE_SIZE, j * SQUARE_SIZE),
+                                    )
+                                else:
+                                    self.screen.blit(
+                                        self.circle_image,
+                                        (i * SQUARE_SIZE, j * SQUARE_SIZE),
+                                    )
+
+            self.display_pieces()
+
+            pygame.display.flip()
+            self.clock.tick(60)
+
+        pygame.quit()
